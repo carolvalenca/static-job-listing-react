8c4cc6c9055e2aabe7a8124242edcc43f7696f53
Carol Valença <caroliny.leandro@ccc.ufcg.edu.br>
2020-08-03 14:20:24 -0300

    Updates

---
+++ precache-manifest.9010b9d3b8ac7239e32c64865653f258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a6faa06bab149fbed0fdb956a975fc",
+    "url": "/static-job-listing-react/index.html"
+  },
+  {
+    "revision": "c8af30637bb66ca3b4f3",
+    "url": "/static-job-listing-react/static/css/main.f92aa030.chunk.css"
+  },
+  {
+    "revision": "4a69f5c5f33a1b770e64",
+    "url": "/static-job-listing-react/static/js/2.347d02c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static-job-listing-react/static/js/2.347d02c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8af30637bb66ca3b4f3",
+    "url": "/static-job-listing-react/static/js/main.92568bd6.chunk.js"
+  },
+  {
+    "revision": "06bf6263a8296e750c7f",
+    "url": "/static-job-listing-react/static/js/runtime-main.1f344c3a.js"
+  }
+]);
